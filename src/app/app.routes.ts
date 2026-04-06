e613e8c224f99d2ad7fa881b801aba0689c79488
lasha <meqo183@gmail.com>
2026-04-07 01:18:16 +0400

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        loadComponent: () => import('./app').then(el => el.App)
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./home/home').then(resp => resp.Home)
+    },
+    {
+        path: 'main',
+        loadComponent: () => import('./main/main').then(resp => resp.Main)
+    },
+    {
+        path: "**"
+        , loadComponent: () => import('./error/error').then(resp => resp.Error)
+    },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './footer/footer';
+import { Header } from './header/header';
+import { Home } from './home/home';
+import { Main } from './main/main';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  imports: [RouterOutlet, Header, Footer, Home, Main]
 })
 export class App {
   protected readonly title = signal('newAng5');
